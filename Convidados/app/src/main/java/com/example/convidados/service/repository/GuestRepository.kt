c344385c00193f3417a29651c5fdb63160898383
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-01 14:20:21 -0300

    select user

--- Convidados/app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
+++ Convidados/app/src/main/java/com/example/convidados/service/repository/GuestRepository.kt
@@ -22,6 +22,47 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
+    fun get(id: Int): GuestModel? {
+
+        var guest: GuestModel? = null
+
+        return try {
+            val db = mGuestDataBaseHelper.readableDatabase
+
+            val projection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            val cursor = db.query(
+                DataBaseConstants.GUEST.TABLE_NAME,
+                projection,
+                selection,
+                args,
+                null,
+                null,
+                null
+            )
+
+            if(cursor != null && cursor.count > 0) {
+                cursor.moveToFirst()
+
+                val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+                val presence = (cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE)) == 1)
+
+                guest = GuestModel(id, name, presence)
+            }
+
+            cursor?.close()
+            guest
+        } catch (e: Exception) {
+            guest
+        }
+    }
+
     fun save(guest: GuestModel): Boolean {
 
         return try {
@@ -32,7 +73,7 @@ class GuestRepository private constructor(context: Context) {
             contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
             db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValues)
             true
-        }catch (e: Exception) {
+        } catch (e: Exception) {
             false
         }
     }
@@ -67,12 +108,12 @@ class GuestRepository private constructor(context: Context) {
             db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValues, selection, args)
 
             true
-        }catch (e: Exception) {
+        } catch (e: Exception) {
             false
         }
     }
 
-    fun delete(id: Int) : Boolean{
+    fun delete(id: Int): Boolean {
         return try {
             val db = mGuestDataBaseHelper.writableDatabase
 
@@ -82,7 +123,7 @@ class GuestRepository private constructor(context: Context) {
             db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
 
             true
-        }catch (e: Exception) {
+        } catch (e: Exception) {
             false
         }
     }
