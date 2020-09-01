0af708f2d7ed661fbf508095ae42d3fe15e8e263
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-01 16:29:30 -0300

    listing all alignments - part 1

--- Convidados/app/src/main/java/com/example/convidados/View/AllGuestsFragment.kt
+++ Convidados/app/src/main/java/com/example/convidados/View/AllGuestsFragment.kt
@@ -4,29 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.convidados.R
+import com.example.convidados.View.adapter.GuestAdapter
 import com.example.convidados.ViewModel.AllGuestsViewModel
 
 class AllGuestsFragment : Fragment() {
 
     private lateinit var allGuestsViewModel: AllGuestsViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        allGuestsViewModel =
-                ViewModelProviders.of(this).get(AllGuestsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        allGuestsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        allGuestsViewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+
+        val root = inflater.inflate(R.layout.fragment_all, container, false)
+
+        // RecyclerView
+        // 1 - Obter a recycler
+        val recycler = root.findViewById<RecyclerView>(R.id.recycler_all_guests )
+
+        // 2 - Definir um layout
+        recycler.layoutManager = LinearLayoutManager(context)
+
+        // 3 - Definir um adapter
+        recycler.adapter = GuestAdapter()
+
         return root
     }
 }
