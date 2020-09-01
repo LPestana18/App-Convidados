5f606427dec5311e478ccc589c5a4e673fd13be5
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-01 13:02:35 -0300

    create database

---
+++ Convidados/app/src/main/java/com/example/convidados/service/DataBaseConstants.kt
@@ -0,0 +1,19 @@
+package com.example.convidados.service
+
+class DataBaseConstants {
+
+
+    /*
+    * Tabelas disponíveis no banco de dados com suas colunas
+    * */
+
+    object GUEST {
+        const val TABLE_NAME = "Guest"
+
+        object COLUMNS {
+            const val ID = "id"
+            const val NAME = "name"
+            const val PRESENCE = "presence"
+        }
+    }
+}
